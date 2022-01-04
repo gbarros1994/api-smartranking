b9c0a4387611af04e2ba6dbedf4665a75261fcc6
gbarros1994 <55881896+gbarros1994@users.noreply.github.com>
2022-01-03 22:07:59 -0300

    criação de interceptors

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CriarJogadorDto } from './dtos/criar-jogador.dto';
 
 @Controller('api/vi/jogadores')
 export class JogadoresController {
   @Post()
-  async criarAtualizarJogador() {
+  async criarAtualizarJogador(@Body() CriarJogadorDto: CriarJogadorDto) {
+    const { email } = CriarJogadorDto;
     return JSON.stringify({
-      nome: 'Gerson',
+      nome: email,
     });
   }
 }
