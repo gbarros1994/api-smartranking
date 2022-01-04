cfbf7c7b09fbac3483b033209ea84b3ea20f2f95
gbarros1994 <55881896+gbarros1994@users.noreply.github.com>
2022-01-03 21:55:52 -0300

    implementando meu primeiro handler

---
+++ src/jogadores/jogadores.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Post } from '@nestjs/common';
+
+@Controller('api/vi/jogadores')
+export class JogadoresController {
+  @Post()
+  async criarAtualizarJogador() {
+    return JSON.stringify({
+      nome: 'Gerson',
+    });
+  }
+}
