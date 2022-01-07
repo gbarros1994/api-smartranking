3ce75c14d60408d24434ba87d7c9f7af6e664c0e
gbarros1994 <55881896+gbarros1994@users.noreply.github.com>
2022-01-07 19:54:14 -0300

    consultar jogadores

--- src/jogadores/jogadores.service.ts
+++ src/jogadores/jogadores.service.ts
@@ -1,15 +1,19 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
 import { Jogador } from './interfaces/jogador.interface';
-import { v4 as uuidv4 } from 'uuid' 
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class JogadoresService {
   private jogadores: Jogador[] = [];
   private readonly logger = new Logger(JogadoresService.name);
+
   async criarAtualizarJogador(criarJogadorDto: CriarJogadorDto): Promise<void> {
-    this.logger.log(`criaJogadorDto: ${criarJogadorDto}`);
     this.criar(criarJogadorDto);
+  }
+
+  async consultarTodosJogadores(): Promise<Jogador[]> {
+    return await this.jogadores;
   }
 
   private criar(criaJogadorDto: CriarJogadorDto): void {
@@ -24,7 +28,7 @@ export class JogadoresService {
       posicaoRanking: 1,
       urlFotoJogador: 'www.google.com.br/foto123.jpg',
     };
-
+    this.logger.log(`criaJogadorDto: ${JSON.stringify(jogador)}`);
     this.jogadores.push(jogador);
   }
 }
