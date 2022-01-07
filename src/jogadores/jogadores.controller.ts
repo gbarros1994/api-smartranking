c66946516ec4c81df4b1e985ddc0442277b96fd0
gbarros1994 <55881896+gbarros1994@users.noreply.github.com>
2022-01-07 18:05:37 -0300

    adição do uuid e criação do service jogador

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,13 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
+import { JogadoresService } from './jogadores.service';
 
 @Controller('api/vi/jogadores')
 export class JogadoresController {
+
+  constructor(private readonly jogadoresService: JogadoresService) {}
+
   @Post()
-  async criarAtualizarJogador(@Body() CriarJogadorDto: CriarJogadorDto) {
-    const { email } = CriarJogadorDto;
-    return JSON.stringify({
-      nome: email,
-    });
+  async criarAtualizarJogador(@Body() criarJogadorDto: CriarJogadorDto) {
+    await this.jogadoresService.criarAtualizarJogador(criarJogadorDto);
   }
 }
--- src/jogadores/jogadores.service.ts
+++ src/jogadores/jogadores.service.ts
@@ -1,10 +1,30 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
+import { Jogador } from './interfaces/jogador.interface';
+import * as uuid from 'uuid';
 
 @Injectable()
 export class JogadoresService {
+  private jogadores: Jogador[] = [];
   private readonly logger = new Logger(JogadoresService.name);
-  async criarAtualizarJogador(criarJogadorDto: CriarJogadorDto) {
-    this.logger.log(`criaJogadorDto: ${criarJogadorDto}`)
+  async criarAtualizarJogador(criarJogadorDto: CriarJogadorDto): Promise<void> {
+    this.logger.log(`criaJogadorDto: ${criarJogadorDto}`);
+    this.criar(criarJogadorDto);
+  }
+
+  private criar(criaJogadorDto: CriarJogadorDto): void {
+    const { nome, telefoneCelular, email } = criaJogadorDto;
+
+    const jogador: Jogador = {
+      _id: uuid(),
+      nome,
+      telefoneCelular,
+      email,
+      ranking: 'A',
+      posicaoRanking: 1,
+      urlFotoJogador: 'www.google.com.br/foto123.jpg',
+    };
+
+    this.jogadores.push(jogador);
   }
 }
