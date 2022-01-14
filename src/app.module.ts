f8c8f642a7126f94dfbcb37d37ee0cfbcd2f6d52
gbarros1994 <55881896+gbarros1994@users.noreply.github.com>
2022-01-13 23:21:52 -0300

    conexao mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { JogadoresModule } from './jogadores/jogadores.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://nestjs:nestjs@cluster0.lyyse.mongodb.net/myFirstDatabase?authSource=admin&replicaSet=Cluster0-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true',
+    ),
+    JogadoresModule,
+  ],
   controllers: [],
   providers: [],
 })
