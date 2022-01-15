72f3441159211fd71c912549c2dbf25003588e8f
gbarros1994 <55881896+gbarros1994@users.noreply.github.com>
2022-01-15 16:10:07 -0300

    add class validator and class transformer

--- src/jogadores/jogadores.module.ts
+++ src/jogadores/jogadores.module.ts
@@ -1,3 +1,5 @@
+/* eslint-disable prettier/prettier */
+
 import { Module } from '@nestjs/common';
 import { JogadoresController } from './jogadores.controller';
 import { JogadoresService } from './jogadores.service';
