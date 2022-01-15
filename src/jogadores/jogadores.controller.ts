26b2afb6e1e8e7ff9a5cac2be6ab20999c2da2c7
gbarros1994 <55881896+gbarros1994@users.noreply.github.com>
2022-01-15 16:29:11 -0300

    pipe on delete

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,7 +1,10 @@
+/* eslint-disable prettier/prettier */
+
 import { Body, Controller, Delete, Get, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
 import { Jogador } from './interfaces/jogador.interface';
 import { JogadoresService } from './jogadores.service';
+import { JogadoresValidacaoParametrosPipe } from './pipes/jogadores-validacao-parametros.pipe';
 
 @Controller('api/v1/jogadores')
 export class JogadoresController {
@@ -25,7 +28,7 @@ export class JogadoresController {
   }
 
   @Delete()
-  async deletarJogador(@Query('email') email: string): Promise<void> {
+  async deletarJogador(@Query('email', JogadoresValidacaoParametrosPipe) email: string): Promise<void> {
     this.jogadoresService.deletarJogador(email);
   }
 }
---
+++ src/jogadores/pipes/jogadores-validacao-parametros.pipe.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+
+import { ArgumentMetadata, BadRequestException, PipeTransform } from "@nestjs/common";
+
+export class JogadoresValidacaoParametrosPipe implements PipeTransform {
+  transform(value: any, metadata: ArgumentMetadata) {
+    if (!value) {
+      throw new BadRequestException(`O valor do parametro ${metadata.data} deve ser informado`);
+    }
+  }
+}
