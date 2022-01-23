90627deec223c80b18f8fbfff237e75b8eaaae4b
gbarros1994 <55881896+gbarros1994@users.noreply.github.com>
2022-01-23 15:19:37 -0300

    categorias

--- src/categorias/categorias.controller.ts
+++ src/categorias/categorias.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { CategoriasService } from './categorias.service';
+import { CriarCategoriaDto } from './dtos/criar-categoria.dto';
+import { Categoria } from './interfaces/categoria.interface';
 
-@Controller('categorias')
-export class CategoriasController {}
+@Controller('api/v1/categorias')
+export class CategoriasController {
+  constructor(private readonly categoriasService: CategoriasService){}
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  async criarCategoria(@Body() criarCategoriaDto: CriarCategoriaDto) : Promise<Categoria> {
+    return await this.categoriasService.criarCategoria(criarCategoriaDto);
+  }
+}
--- src/categorias/categorias.service.ts
+++ src/categorias/categorias.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Categoria } from './interfaces/categoria.interface';
+import { Model } from 'mongoose';
+import { CriarCategoriaDto } from './dtos/criar-categoria.dto';
 
 @Injectable()
-export class CategoriasService {}
+export class CategoriasService {
+
+  constructor(@InjectModel('Categoria') private readonly categoriaModel: Model<Categoria>) {}
+    
+    async criarCategoria(criarCategoriaDto: CriarCategoriaDto): Promise<Categoria> {
+      const { categoria } = criarCategoriaDto;
+
+      const categoriaEncontrada = await this.categoriaModel.find({categoria}).exec();
+
+      if (categoriaEncontrada) {
+        throw new BadRequestException(`Categoria ${categoria} já cadastrada!`)
+      }
+
+      const categoriaCriada = new this.categoriaModel(criarCategoriaDto);
+      return await categoriaCriada.save();
+    }
+}
